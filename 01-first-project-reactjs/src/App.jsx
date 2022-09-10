5edc4a015a6cdb5ac572905937b33d0a582d2ad8
MUSQUITU <camilinhaalmeida55@gmail.com>
2022-09-10 16:41:30 -0300

    first files

---
+++ 01-first-project-reactjs/src/App.jsx
@@ -0,0 +1,24 @@
+import { Header } from './components/Header'
+
+import styles from './App.module.css'
+
+import './global.css'
+import { Sidebar } from './components/Sidebar'
+
+export function App() {
+  return (
+   <div>
+    <Header />
+
+    <div className={styles.wrapper}> 
+      <Sidebar />
+      <main>
+
+      </main>
+
+    </div>
+   </div>
+  )
+}
+
+
