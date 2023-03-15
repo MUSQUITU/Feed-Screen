a7a363c9dff21d101cfc1378f00d5ae183485e9b
MUSQUITU <camilinhaalmeida55@gmail.com>
2023-03-15 16:55:23 -0300

    :lipstick: add new files

--- 01-first-project-reactjs/src/App.jsx
+++ 01-first-project-reactjs/src/App.jsx
@@ -15,6 +15,7 @@ export function App() {
       <Sidebar />
       <main>
       <Post />
+      <Post />
       </main>
 
     </div>
--- 01-first-project-reactjs/src/components/Post.jsx
+++ 01-first-project-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -13,8 +14,32 @@ export function Post() {
           </div>
         </div>
 
-        <time></time>
+        <time title='07 de dezembro de 2022' dateTime='2022-12-07 12:01'>Publicado há 1h</time>
       </header>
+
+      <div className={styles.content}>
+      <p>Fala galeraa 👋</p>
+      <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+      <p>👉<a href="https://www.instagram.com/camis.js" target='_blank'>camis.js </a></p>
+      <p> <a href="##">#novoprojeto</a> {' '}
+      <a href='##'> #nlw </a>{' '}
+      <a href='##'>#rocketseat</a></p>
+      </div>
+
+    <form className={styles.commentForm}>
+      <strong>Deixe seu feedback </strong>
+      <textarea
+        placeholder='Deixe um comentário'
+      />
+       <footer>
+       <button type='submit'>Publicar</button>
+        </footer> 
+    </form>
+
+    <div className={styles.commentList}>
+      <Comment/>
+      <Comment/>
+    </div>
     </article>
     
   )
